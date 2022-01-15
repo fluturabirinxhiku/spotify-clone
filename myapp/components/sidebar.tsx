3f4e0e8b6be5b68c0d3c6d6467855c85e978c161
Flutura Birinxhiku <fluturabirinxhiku@gmail.com>
2022-01-15 02:58:37 +0100

    Added the music menu and playlists in the sidebar

--- myapp/components/sidebar.tsx
+++ myapp/components/sidebar.tsx
@@ -23,10 +23,13 @@ const navMenu = [
   { label: "Home", icon: MdHome, route: "/" },
   { label: "Search", icon: MdSearch, route: "/search" },
   { label: "Your Library", icon: MdLibraryMusic, route: "/library" },
-  { label: "Create Playlist", icon: MdPlaylistAdd, route: "/playlist/create" },
-  { label: "Liked Songs", icon: MdFavorite, route: "/songs" },
 ];
 
+const musicMenu = [
+  { label: "Create Playlist", icon: MdPlaylistAdd, route: "/" },
+  { label: "Liked Songs", icon: MdFavorite, route: "/songs" },
+];
+const playlists = new Array(50).fill(1).map((_, i) => `Playlist ${i + 1}`);
 const Sidebar = () => {
   return (
     <Box
@@ -36,9 +39,9 @@ const Sidebar = () => {
       paddingX="5px"
       color="gray"
     >
-      <Box paddingY="20px ">
-        <Box width="120px" marginBottom="20px" paddingLeft="20px">
-          <NextImage src="/logo.svg" width={120} height={60} />
+      <Box paddingY="20px" height="100%">
+        <Box width="120px" marginBottom="10px" paddingLeft="20px">
+          <NextImage src="/logo.svg" width={120} height={40} />
         </Box>
         <Box marginBottom="20px">
           <List spacing={2}>
@@ -54,6 +57,40 @@ const Sidebar = () => {
                       />
                       {menuItem.label}
                     </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box marginTop="20px">
+          <List spacing={2}>
+            {musicMenu.map((menuItem) => (
+              <ListItem paddingX="20px" fontSize="16px" key={menuItem.label}>
+                <LinkBox>
+                  <NextLink href={menuItem.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon
+                        as={menuItem.icon}
+                        color="white"
+                        marginRight="20px"
+                      />
+                      {menuItem.label}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Divider color="gray.800" marginY="5px" />
+        <Box height="50%" overflowY="auto" paddingY="20px">
+          <List spacing={2}>
+            {playlists.map((playlist) => (
+              <ListItem paddingX="20px" key={playlist}>
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{playlist}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
